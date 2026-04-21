70e96a5ab2b95f43a15ec270c158f4caa8b3747a
Edouard <edouardtuyubahe@gmail.com>
2026-04-21 14:55:59 +0200

    stage 4: views, url routing, function and class based views

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('hello/', views.hello, name='hello'),
+    path('posts/<int:id>/', views.get_post, name='get-post'),
+    path('posts/', views.get_all_posts, name='get-all-posts'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from .models import Post
+import json
 
-# Create your views here.
+
+def hello(request):
+    return HttpResponse('Hello Django!')
+
+
+def get_post(request, id):
+    post = Post.objects.get(id=id)
+    return JsonResponse({
+        'id': post.id,
+        'title': post.title,
+        'content': post.content,
+        'is_published': post.is_published,
+    })
+
+
+def get_all_posts(request):
+    posts = Post.objects.all()
+    posts_list = list(posts.values('id', 'title', 'content', 'is_published'))
+    return JsonResponse({'posts': posts_list})
+
+
+@csrf_exempt
+def posts(request):
+    if request.method == 'GET':
+        all_posts = list(Post.objects.values('id', 'title', 'content'))
+        return JsonResponse({'posts': all_posts})
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        post = Post.objects.create(
+            title=data['title'],
+            content=data['content'],
+            is_published=data.get('is_published', False)
+        )
+        return JsonResponse({'id': post.id, 'title': post.title}, status=201)
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class PostsView(View):
+    def get(self, request):
+        all_posts = list(Post.objects.values('id', 'title', 'content'))
+        return JsonResponse({'posts': all_posts})
+
+    def post(self, request):
+        data = json.loads(request.body)
+        post = Post.objects.create(
+            title=data['title'],
+            content=data['content']
+        )
+        return JsonResponse({'id': post.id}, status=201)
